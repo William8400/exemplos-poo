77c3d612ddebfeb9d171a5029247d835d9f0df7d
William8400 <wdonato02@gmail.com>
2025-10-21 17:23:07 -0300

    Método construtor ainda incompleto

--- src/Cliente.php
+++ src/Cliente.php
@@ -3,4 +3,11 @@ class Cliente {
     public string $nome;
     public int $idade;
     public string $email;
+
+    /* Método construtor: responsável por 'construir' um objeto COM seus dados no momento da criação */
+
+    public function __construct( string $valorDoNome, int $valorDaIdade, string $valorDoEmail)
+    {
+        
+    }
 }
