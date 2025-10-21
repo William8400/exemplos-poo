db2f145fdac66917c5ef00b1c45bf1fa1dc507da
William8400 <wdonato02@gmail.com>
2025-10-21 14:41:34 -0300

    lendo os dados do objeto

--- index.php
+++ index.php
@@ -7,9 +7,14 @@ $ClienteA = new Cliente();
 $ClienteB = new Cliente();
 
 // Atribuindo valores ao objetos $ClienteA
-$ClienteA->nome = "William";
+$ClienteA->nome = "Arroba";
 $ClienteA->idade = 21;
 $ClienteA->email = "arroba@gmail.com";
+
+$ClienteB->nome = "Magnus";
+$ClienteB->idade = 23;
+$ClienteB->email = "magnus@gmail.com"
+
 
 
 ?>
@@ -19,12 +24,32 @@ $ClienteA->email = "arroba@gmail.com";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exemplos</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>Exemplos de PHP com POO</h1>
     <hr>
 
     <h2>Trabalhando com classes e objetos</h2>
+
+    <h3>Acessando/lendo os dados dos objetos</h3>
+    <h4>Cliente A</h4>
+    <ul>
+        <li><b>Nome:</b> <?=$ClienteA->nome?> </li>
+        <li><b>Idade:</b> <?=$ClienteA->idade?> </li>
+        <li><b>E-mail:</b> <?=$ClienteA->email?> </li>
+    </ul>
+
+    <section>
+    <div class="clienteb">
+        <h4>ClienteB</h4>
+        <ul>
+            <li><b>Nome:</b> <?=$ClienteB->nome?></li>
+            <li><b>Idade:</b> <?=$ClienteB->idade?></li>
+            <li><b>E-mail:</b> <?=$ClienteB->email?></li>
+        </ul>
+    </div>
+    </section>
 
     <h3>Visualizando a estrutura dos objetos</h3>
     <pre><?=var_dump($ClienteA, $ClienteB)?></pre>
