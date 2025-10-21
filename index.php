803ce3a793bc2165755b271ebeb78bc1b76b5f5a
William8400 <wdonato02@gmail.com>
2025-10-21 14:56:53 -0300

    retornando o método

--- index.php
+++ index.php
@@ -33,23 +33,11 @@ $ClienteB->email = "magnus@gmail.com"
     <h2>Trabalhando com classes e objetos</h2>
 
     <h3>Acessando/lendo os dados dos objetos</h3>
-    <h4>Cliente A</h4>
-    <ul>
-        <li><b>Nome:</b> <?=$ClienteA->nome?> </li>
-        <li><b>Idade:</b> <?=$ClienteA->idade?> </li>
-        <li><b>E-mail:</b> <?=$ClienteA->email?> </li>
-    </ul>
 
-    <section>
-    <div class="clienteb">
-        <h4>ClienteB</h4>
-        <ul>
-            <li><b>Nome:</b> <?=$ClienteB->nome?></li>
-            <li><b>Idade:</b> <?=$ClienteB->idade?></li>
-            <li><b>E-mail:</b> <?=$ClienteB->email?></li>
-        </ul>
-    </div>
-    </section>
+    <?=$ClienteA->mostrarDados()?>
+    
+    <?=$ClienteB->mostrarDados()?>
+
 
     <h3>Visualizando a estrutura dos objetos</h3>
     <pre><?=var_dump($ClienteA, $ClienteB)?></pre>
