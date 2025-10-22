23ce854bbf9a26bb19812e08fe8ce05e06a12c9d
William8400 <wdonato02@gmail.com>
2025-10-22 14:29:52 -0300

    criando atributo telefone

--- src/Cliente.php
+++ src/Cliente.php
@@ -4,12 +4,20 @@ class Cliente {
     public int $idade;
     public string $email;
 
+    // Telefone é opcional, ou seja, caso não seja informado ficará valendo null 
+    public ?string $telefone; // ? indica que este atributo PODE ser NULL
+
     /* Método construtor: responsável por 'construir' um objeto COM seus dados no momento da criação */
 
-    public function __construct( string $valorDoNome, int $valorDaIdade, string $valorDoEmail)
+    public function __construct( 
+        string $valorDoNome, 
+        int $valorDaIdade, 
+        string $valorDoEmail, 
+        ?string $valorDoTelefone = null) // null como valor padrão
     {
         $this->nome = $valorDoNome;
         $this->idade = $valorDaIdade;
         $this->email = $valorDoEmail;
+        $this->telefone = $valorDoTelefone;
     }
 }
