be652dabfece1af2deae8748e8aba979cda3a517
William8400 <wdonato02@gmail.com>
2025-10-22 14:49:48 -0300

    modificando a classe livro para método construtor

--- src/Livro.php
+++ src/Livro.php
@@ -3,4 +3,11 @@ class Livro {
     public string $titulo;
     public string $autor;
     public int $quantidade;  
+
+    public function __construct(string $valorDoTitulo, string $valorDoAutor , int $valorDaQuantidade){
+        $this->$valorDoTitulo;
+        $this->$valorDoAutor;
+        $this->$valorDaQuantidade;
+}  
 }
+
