4f8919b5be184ebdd7d745bef2becf5037e6f853
William8400 <wdonato02@gmail.com>
2025-10-28 16:25:21 -0300

    adaptando meu modo construtor

--- src/Livro.php
+++ src/Livro.php
@@ -5,9 +5,9 @@ class Livro {
     private ?int $quantidade;  
 
     public function __construct(string $valorDoTitulo, string $valorDoAutor, ?int $valorDaQuantidade = null){
-        $this->titulo = $valorDoTitulo;
-        $this->autor = $valorDoAutor;
-        $this->quantidade = $valorDaQuantidade;
+        $this->setTitulo($valorDoTitulo);
+        $this->setAutor($valorDoAutor);
+        $this->setQuantidade($valorDaQuantidade);
 }  
     /* Usamos um if para, se houver páginas (ou seja, não for null), faremos aparecer o prágrafo com o dado. */
     public function verificarTitulo():void{
@@ -28,5 +28,34 @@ class Livro {
     public function getQuantidade():?int {
         return $this->quantidade;
     }
+
+    private function setTitulo(string $valorTitulo):void {
+        if (empty($valorTitulo) || mb_strlen($valorTitulo) < 4) {
+            echo "<p style='color:red'>Titulo  é Obrigatório!</p>";
+        } else {
+            $this->titulo = $valorTitulo;
+        }
+    }
+
+    private function setAutor(string $valorAutor):void {
+        if (empty($valorAutor) || mb_strlen($valorAutor) < 3) {
+            echo "<p style='color:red'> Autor é Obrigatório!</p>";
+        } else {
+            $this->autor = $valorAutor;
+        }
+    }
+
+    public function setQuantidade(?int $valorQuantidade):void {
+
+        if ($valorQuantidade < 0) {
+           echo "<p style='color:red'>Quantidade não pode ser negativa!</p>";
+        } else {
+            $this->quantidade = $valorQuantidade;
+        }
+    }
+
+
+
+    
 }
 
